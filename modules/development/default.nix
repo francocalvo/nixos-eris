9d4651181b677d1ea8ad600ca33db79c2c255268
francocalvo <dev@francocalvo.ar>
2023-02-21 15:30:23 -0300

    add vscode to dev tools

--- modules/development/default.nix
+++ modules/development/default.nix
@@ -3,7 +3,7 @@
 {
   imports = [ ./nodejs.nix ./python.nix ];
 
-  home.packages = with pkgs; [ git zathura dbeaver clang ];
+  home.packages = with pkgs; [ git zathura dbeaver clang vscode ];
 
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
