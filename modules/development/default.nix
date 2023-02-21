04cb17b4f628c5d2686d70415c6fab9e9e3d5b8f
francocalvo <dev@francocalvo.ar>
2023-02-21 15:54:01 -0300

    switch clang for gcc

--- modules/development/default.nix
+++ modules/development/default.nix
@@ -3,7 +3,15 @@
 {
   imports = [ ./nodejs.nix ./python.nix ];
 
-  home.packages = with pkgs; [ git zathura dbeaver clang vscode ];
+  home.packages = with pkgs; [
+    git
+    zathura
+    gcc
+    dbeaver
+    vscode
+    llvm
+    libcxx
+  ];
 
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
