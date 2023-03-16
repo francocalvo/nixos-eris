e76e5b025a2fc3558c2fd6cc52dc15a0c013c156
francocalvo <dev@francocalvo.ar>
2023-03-16 17:14:56 -0300

    add new colorscheme to nvim

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,42 +1,45 @@
 if vim.g.vscode then
-  require "user.alpha"                      -- OK
-  require "user.nvim-webdev-icons"          -- OK - updated up to 5b240a9
-  require "user.whichkey"                   -- OK
-  require "user.plugins"                    -- OK - See below
-  require "user.options"                    -- OK - updated up to 2022/09/12
-  require "user.illuminate"                 -- Not updated
-  require "user.colorscheme"                -- OK
-  require "user.cmp"                        -- OK - Pretty similar, without tabnine
-  require "user.treesitter"                 -- OK - without textobjects part at the end
-  require "user.keymaps"                    -- OK
-  require "user.autopairs"                  -- OK
-  require "user.telescope"                  -- OK - updated up to  5b240a9
+  vim.cmd [[ xmap gc  <Plug>VSCodeCommentary ]]
+  vim.cmd [[ nmap gc  <Plug>VSCodeCommentary ]]
+  vim.cmd [[ omap gc  <Plug>VSCodeCommentary ]]
+  vim.cmd [[ nmap gcc  <Plug>VSCodeCommentaryLine ]]
 else
-  require "user.lsp"                        -- OK - updated up to 2022/09/12
-  require "user.autotag"                    -- OK
-  require "user.comment"                    -- OK
-  require "user.gitsigns"                   -- OK
-  require "user.nvim-tree"                  -- OK - updated up to  5b240a9
-  require "user.lualine"                    -- OK - updated up to 5b240a9
-  require "user.toggleterm"
-  require "user.project"                    -- OK
-  require "user.impatient"                  -- OK
-  require "user.indentline"                 -- OK - updated up to 5b240a9
-  require "user.autocommands"               -- updated up to 2022/09/12 
-  require "user.neoscroll"                  -- OK
-  --[[ require "user.todo-comments"              -- NOT OK ]]
-  require "user.numb"                       -- OK
-  require "user.vimwiki"                    -- TODO: Finish setup
-  require "user.functions"                  -- updated up to  5b240a9
-  require "user.navic"                      -- updated up to  5b240a9
-  require "user.winbar"                     -- OK - updated up to 2022/09/12
-  require "user.dap"
-  require "user.cybu"
-  require "user.vimtex"
-  require "user.bufferline"                 -- OK - updated up to 2022/09/12
-  --[[ require "user.copilot" ]]
+	require("user.alpha") -- OK
+	require("user.nvim-webdev-icons") -- OK - updated up to 5b240a9
+	require("user.whichkey") -- OK
+	require("user.plugins") -- OK - See below
+	require("user.options") -- OK - updated up to 2022/09/12
+	require("user.illuminate") -- Not updated
+	require("user.colorscheme") -- OK
+	require("user.cmp") -- OK - Pretty similar, without tabnine
+	require("user.treesitter") -- OK - without textobjects part at the end
+	require("user.keymaps") -- OK
+	require("user.autopairs") -- OK
+	require("user.telescope") -- OK - updated up to  5b240a9
+	require("user.lsp") -- OK - updated up to 2022/09/12
+	require("user.autotag") -- OK
+	require("user.comment") -- OK
+	require("user.gitsigns") -- OK
+	require("user.nvim-tree") -- OK - updated up to  5b240a9
+	require("user.lualine") -- OK - updated up to 5b240a9
+	require("user.toggleterm")
+	require("user.project") -- OK
+	require("user.impatient") -- OK
+	require("user.indentline") -- OK - updated up to 5b240a9
+	require("user.autocommands") -- updated up to 2022/09/12
+	require("user.neoscroll") -- OK
+	--[[ require "user.todo-comments"              -- NOT OK ]]
+	require("user.numb") -- OK
+	require("user.vimwiki") -- TODO: Finish setup
+	require("user.functions") -- updated up to  5b240a9
+	require("user.navic") -- updated up to  5b240a9
+	require("user.winbar") -- OK - updated up to 2022/09/12
+	require("user.dap")
+	require("user.cybu")
+	require("user.vimtex")
+	require("user.bufferline") -- OK - updated up to 2022/09/12
+	--[[ require "user.copilot" ]]
 end
-
 
 -- Not added nor checked from his repo:
 -- - lsp-inlayhints
--- dotfiles/nvim/lua/user/colorscheme.lua
+++ dotfiles/nvim/lua/user/colorscheme.lua
@@ -1,5 +1,5 @@
 --[[ local colorscheme = "darkplus" ]]
-local colorscheme = "tokyonight"
+local colorscheme = "tokyonight-night"
 
 vim.g.onedarker_italic_keywords = false
 
