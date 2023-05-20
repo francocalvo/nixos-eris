960b87220fb84a70e850869834391e771ca9a112
francocalvo <dev@francocalvo.ar>
2023-05-19 22:39:42 -0300

    create default configuration for hosts

--- default.nix
+++ default.nix
@@ -5,16 +5,20 @@ in {
   imports = [ inputs.home-manager.nixosModules.home-manager ]
     ++ my.mapModulesRec' ./modules import;
 
-  # ‹nix flake check› bypasses, can be changed in the actual hosts
-  # config, useful for testing
-
+  # Keep the system clean
   nix = {
-    autoOptimiseStore = true;
-    extraOptions = "experimental-features = nix-command flakes";
-    binaryCaches = [ "https://nix-community.cachix.org" ];
-    binaryCachePublicKeys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+  };
+
+  # Keep the system updated
+  system.autoUpgrade = {
+    enable = true;
+    channel = "https://nixos.org/channels/nixos-unstable";
   };
 
   # Use the GRUB 2 boot loader.
@@ -26,7 +30,18 @@ in {
     useOSProber = true;
   };
 
-  environment.systemPackages = with pkgs; [ curl git wget neovim ];
-
   i18n.defaultLocale = "en_US.UTF-8";
+
+  # Base packages
+  environment.systemPackages = with pkgs; [
+    bind
+    cached-nix-shell
+    git
+    curl
+    vim
+    neovim
+    wget
+    gnumake
+    unzip
+  ];
 }
