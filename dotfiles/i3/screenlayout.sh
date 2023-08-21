4c08a9791e6951d7e4507323db5afb666d1b13ca
francocalvo <dev@francocalvo.ar>
2023-08-21 19:32:27 -0300

    fix layouts for i3

--- dotfiles/i3/screenlayout.sh
+++ dotfiles/i3/screenlayout.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output DisplayPort-1 --primary --mode 1920x1080 -r 144.00 --pos 2560x0 --rotate normal --output DisplayPort-0 --primary --mode 1920x1080 --pos 4480x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 2560x1080 --pos 0x0 --rotate normal
+xrandr --output DP-1 --mode 1920x1080 --pos 1080x0 r 144.00 --rotate normal #--output DP-2 --mode 1920x1080 --pos 0x0 --rotate left --output DP-3 --mode 2560x1080 --pos 3000x0 --rotate normal
 feh --no-fehbg --bg-center '/home/calvo/.config/i3/wal.jpg' 
