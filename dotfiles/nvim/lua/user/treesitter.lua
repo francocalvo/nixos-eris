889cd232c137458c02decbadb129c370c8cc3cc4
francocalvo <dev@francocalvo.ar>
2024-07-29 10:22:39 -0300

    fix neovim 'autotag' plugin config

--- dotfiles/nvim/lua/user/treesitter.lua
+++ dotfiles/nvim/lua/user/treesitter.lua
@@ -9,10 +9,6 @@ function M.config()
     ensure_installed = { "python", "lua", "javascript", "typescript", "prisma" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
     ignore_install = { "" }, -- List of parsers to ignore installing
-    autotag = {
-      enable = true,
-      disable = { "xml" },
-    },
     highlight = {
       enable = true, -- false will disable the whole extension
       disable = { "css" }, -- list of language that will be disabled
